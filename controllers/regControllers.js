52fcbdf245a725538c8115f56e528492592830ae
NAFIZ009 <jalalahmednafiz@gmail.com>
2023-11-05 03:37:29 +0600

    reg

--- controllers/regControllers.js
+++ controllers/regControllers.js
@@ -2,15 +2,15 @@ const Users = require('../models/Users');
 const { v4: uuidv4 } = require('uuid');
 const bcrypt = require('bcrypt');
 
-
+const salt =bcrypt.genSaltSync(10);
 exports.createUser = async (req, res) => {
-    console.log(req.body);
-    // const { username, password } = req.body;
-    // password = await bcrypt.hash(password, 2231);
-    // try {
-    //   const user = await Users.create({ username, password });
-    //   res.status(201).json(user);
-    // } catch (error) {
-    //   res.status(400).send('Error creating user');
-    // }
+    let { username, password } = req.body;
+    password = await bcrypt.hash(password, salt);
+    try {
+      const user = await Users.create({ name:username, password });
+      res.status(201).json(user);
+    } catch (error) {
+        console.log(error);
+      res.status(400).send('Error creating user');
+    }
 };
--- routes/regRoutes.js
+++ routes/regRoutes.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const regControllers = require('../controllers/regControllers');
 const regRoute=express();
 
 regRoute.use(express.urlencoded({ extended: true}));
+
+regRoute.use(fileUpload());
 
 regRoute.get('/',(req, res) => {
     
 });
 
 
-regRoute.post('/create',(req,res)=>{
-    console.log(req.username);
-},regControllers.createUser);
+regRoute.post('/create',regControllers.createUser);
 
 module.exports=regRoute;
