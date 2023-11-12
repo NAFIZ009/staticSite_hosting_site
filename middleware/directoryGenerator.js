4078b68dc0b7bdb7e502e3cba0965e2ed7ce69bd
NAFIZ009 <jalalahmednafiz@gmail.com>
2023-11-12 18:20:35 +0600

    test

--- middleware/directoryGenerator.js
+++ middleware/directoryGenerator.js
@@ -5,14 +5,14 @@ const directoryGenerator=(req,res,next)=>{
     const {userId}=jwt.verify(req.cookies.token,process.env.SECRET_KEY);
     //create a folder according to the userId
     const siteDirectory = `uploads/${userId}`;
-    fs.access(siteDirectory, fs.constants.F_OK, (err) => {
-        if (err) {
-            fs.mkdirSync(siteDirectory);
+    fs.exists(siteDirectory, (exists) => {
+        if (exists) {
+            console.log('directory already exists')
             req.siteDirectory = siteDirectory;
             req.siteID = userId;
             next();
         } else {
-            console.log('directory already exists')
+            fs.mkdirSync(siteDirectory);
             req.siteDirectory = siteDirectory;
             req.siteID = userId;
             next();
